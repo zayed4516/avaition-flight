8afdc63eb779296873c4df1bc1256208a3af2bff
__Mohamed-Zayed__ <143558508+zayed4516@users.noreply.github.com>
2024-09-17 20:19:31 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ with st.sidebar:
 
 
     st.sidebar.write("")
-    st.sidebar.markdown("Made by [Hussein zayed](https://github.com/HusseinZayed)")
+    st.sidebar.markdown("Made by [Mohamed zayed](https://github.com/zayed4516/)")
 
     # filtering Function
 def filter(airline, source, destination, add_info):
